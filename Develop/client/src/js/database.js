45b005360578bdf5236adb0d7aad468675807eab
BraideyH <braideyhooper@gmail.com>
2024-02-24 02:17:56 -0600

    attempted various fixes to get the build to run. running into an error that is preventing the __WB_Manifest from being found in SW source.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,32 +15,27 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.error('putDb not implemented');
-  const jateDb = await openDB('jate', 1);
+  const jateDB = await openDB('jate', 1);
 
   const text = jateDB.transaction("jate", "readwrite");
 
-  const store = tx.objectStore('jate');
+  const store = text.objectStore('jate');
 
   const request = store.put({ id: 1, value: content });
 
   const result = await request;
-  console.log('result.value', result);
+  console.log(`result.value`, result);
   return result;
 }
-  const jateDB = await openDB("jate");
-  const text = jateDB.transaction("jate", "readwrite");
-  const store = text.objectStore("jate");
-  const request = store.put({ id: 1, value: content });
-  const result = await request;
-  console.log("Stored data in the database!", result);
+ 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error('getDb not implemented');
   const jateDB = await openDB("jate");
 
-  const tx = jateDb.transaction('jate', 'readonly');
+  const text = jateDB.transaction('jate', 'readonly');
 
-  const store = tx.objectStore('contact');
+  const store = text.objectStore('contact');
 
   const request = store.getAll();
 
